70c0552cac7bf8dedbbbb19263113ba954ec7fd5
DESKTOP-K72SDNP\Chitesh <thechitesh@gmail.com>
2024-08-07 21:58:48 +0200

    LCM : updating to java 17 and Spring boot 3

--- stock-service/src/main/java/com/myorg/stock/repository/StockRepository.java
+++ stock-service/src/main/java/com/myorg/stock/repository/StockRepository.java
@@ -3,11 +3,11 @@ package com.myorg.stock.repository;
 import com.myorg.stock.model.StockEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StockRepository extends PagingAndSortingRepository<StockEntity, Long> {
+public interface StockRepository extends JpaRepository<StockEntity, Long> {
 
   Page<StockEntity> findAll(Pageable pageable);
 }
